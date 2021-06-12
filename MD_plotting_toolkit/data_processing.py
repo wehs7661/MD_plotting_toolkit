9c24ae3e8afe29cd0693b930ea59cd0d8f516cbe
Wei-Tse Hsu <Wei-TseHsu@MacBook-Pro-2.local>
2021-06-12 17:36:00 -0600

    Configured flake8 and reformatted docstrings

--- MD_plotting_toolkit/data_processing.py
+++ MD_plotting_toolkit/data_processing.py
@@ -3,7 +3,7 @@
 #    MD_plotting_toolkit,                                                 #
 #    a python package to visualize the results obtained from MD           #
 #                                                                         #
-#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                       #                                                             #
+#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                       #
 #    Copyright (c) 2021 University of Colorado Boulder                    #
 #                                                                         #
 ###########################################################################
@@ -11,8 +11,8 @@
 The `data_processing` module provides functions for processing data.
 """
 import numpy as np
-import MD_plotting_toolkit.plotting_utils as plotting_utils
-from MD_plotting_toolkit.utils import *
+import plotting_utils
+import utils
 
 
 def deduplicate_data():
@@ -21,34 +21,37 @@ def deduplicate_data():
 
 def read_2d_data(f_input, col_idx=1):
     """
-    This function reads in any input file that is readable by np.loadtxt or the 
-    ones that follow the GROMACS xvg format. It returns a 2 x n array, where 
-    n is the number of data points of each variable. By default, the independent 
+    This function reads in any input file that is readable by np.loadtxt or the
+    ones that follow the GROMACS xvg format. It returns a 2 x n array, where
+    n is the number of data points of each variable. By default, the independent
     variable and the dependent variable are the data of the first and second column.
-    The user can decide which column to read as the dependent variable. 
+    The user can decide which column to read as the dependent variable.
 
     Parameters
     ----------
-    f_input (str): The filename of the input file. 
-    col_idx (int): The index (starting from 0) of the column to be read as the 
-                   dependent variable. 
+    f_input : str
+        The filename of the input file.
+    col_idx : int
+        The index (starting from 0) of the column to be read as the dependent variable.
 
     Returns
     -------
-    x_data (np.array): The data of independent variable read from the input file. 
-    y_data (np.array): The data of dependent variable read from the input file. 
+    x_data : np.array
+        The data of independent variable read from the input file.
+    y_data : np.array
+        The data of dependent variable read from the input file.
     """
     try:
         data = np.transpose(np.loadtxt(f_input))
         x_data, y_data = data[0], data[col_idx]
     except ValueError:
         x_data, y_data = [], []
-        infile = open(f_input, 'r')
+        infile = open(f_input, "r")
         lines = infile.readlines()
         infile.close()
 
         for line in lines:
-            if '#' not in line and '@' not in line:
+            if "#" not in line and "@" not in line:
                 x_data.append(float(line.split()[0]))
                 y_data.append(float(line.split()[col_idx]))
 
@@ -59,44 +62,54 @@ def read_2d_data(f_input, col_idx=1):
 
 def scale_data(data, conversion=None, factor=None, T=None):
     """
-    This function scales the input data according to the desired unit conversion 
+    This function scales the input data according to the desired unit conversion
     or scaling factor specified by the user.
 
     Parameters
     ----------
-    data  (np.array): The input data to be scaled. 
-    conversion (str): The string describing what units the conversion should 
-                      be carried out between. Available conversions include "ps to ns",
-                      "kT to kJ/mol", "kT to kcal/mol", "kJ/mol to kcal/mol", "degree to radian",
-                      and their respective conversion in the opposite direction. Note that 
-                      the list of available conversions does not mean to be comprehensive since
-                      the user could just specify the scaling factor. 
-    factor   (float): The scaling factor for data scaling. 
-    T        (float): The temperature to be considered to convert 
-                      energy units to kT or vice versa.
+    data : np.array
+        The input data to be scaled.
+    conversion : str
+        The string describing what units the conversion should be carried out between.
+        Available conversions include "ps to ns", "kT to kJ/mol", "kT to kcal/mol",
+        "kJ/mol to kcal/mol", "degree to radian", and their respective conversion in
+        the opposite direction. Note that the list of available conversions does not
+        mean to be comprehensive since the user could just specify the scaling factor.
+    factor : float
+        The scaling factor for data scaling.
+    T : float
+        The temperature to be considered to convert energy units to kT or vice versa.
 
     Returns
     -------
-    data (np.array): The processed data
+    data : np.array
+        The processed data
     """
     c1 = 1.38064852 * 6.022 * T / 1000  # multiply to convert from kT to kJ/mol
     c2 = np.pi / 180  # multiply to convert from degree to radian
     c3 = 0.239005736  # multiply to convert from J to cal (or kJ/mol to kcal/mol)
 
     conversion_dict = {
-        'ns to ps': 1000, 'ps to ns': 1 / 1000,
-        'kT to kJ/mol': c1, 'kJ/mol to kT': 1 / c1,
-        'kT to kcal/mol': c1 * c3, 'kcal/mol to kT': 1 / (c1 * c3),
-        'kJ/mol to kcal/mol': c3, 'kcal/mol to kJ/mol': 1 / c3,
-        'degree to radian': c2, 'radian to degree': 1 / c2
+        "ns to ps": 1000,
+        "ps to ns": 1 / 1000,
+        "kT to kJ/mol": c1,
+        "kJ/mol to kT": 1 / c1,
+        "kT to kcal/mol": c1 * c3,
+        "kcal/mol to kT": 1 / (c1 * c3),
+        "kJ/mol to kcal/mol": c3,
+        "kcal/mol to kJ/mol": 1 / c3,
+        "degree to radian": c2,
+        "radian to degree": 1 / c2,
     }
 
     if conversion is not None:
         if conversion in conversion_dict:
             data *= conversion_dict[conversion]
         else:
-            raise ParameterError('The specified conversion is not available. \
-                                 Try using the scaling factor. ')
+            raise utils.ParameterError(
+                "The specified conversion is not available. \
+                                 Try using the scaling factor. "
+            )
 
     if factor is None:
         factor = 1
@@ -108,24 +121,28 @@ def scale_data(data, conversion=None, factor=None, T=None):
 
 def slice_data(data, truncate=None, retain=None):
     """
-    This function slices the data given the truncation fraction or the fraction of data 
-    to be retained. The values should be with 0 to 100 (units: percent). 
+    This function slices the data given the truncation fraction or the fraction of data
+    to be retained. The values should be with 0 to 100 (units: percent).
 
     Parameters
     ----------
-    data  (np.array): The input data to be sliced.
-    truncate (float): The fraction of data to be truncated from the beginning.
-    retain   (float): The fraction of data to be retained from the beginning.
+    data : np.array
+        The input data to be sliced.
+    truncate : float
+        The fraction of data to be truncated from the beginning.
+    retain : float
+        The fraction of data to be retained from the beginning.
 
     Returns
     -------
-    data (data): The processed data.
+    data : data
+        The processed data.
     """
     if truncate is not None:
         data = data[int(0.01 * float(truncate) * len(data)):]
 
     if retain is not None:
-        data = data[:int(0.01 * float(retain) * len(data))]
+        data = data[: int(0.01 * float(retain) * len(data))]
 
     return data
 
@@ -136,25 +153,33 @@ def analyze_data(x, y, x_label, y_label, outfile):
 
     Parameters
     ----------
-    x  (np.array): The data of variable x.
-    y  (np.array): The data of variable y.
-    x_label (str): The label of the x-axis.
-    y_label (str): The lable of the y-axis.
+    x : np.array
+        The data of variable x.
+    y : np.array
+        The data of variable y.
+    x_label : str
+        The label of the x-axis.
+    y_label : str
+        The lable of the y-axis.
     """
-    L = Logging(outfile)
+    L = utils.Logging(outfile)
     x_var, x_unit = plotting_utils.identify_var_units(x_label)
     y_var, y_unit = plotting_utils.identify_var_units(y_label)
 
     y_avg = np.mean(y)
     y2_avg = np.mean(np.power(y, 2))
     RMSF = np.sqrt((y2_avg - y_avg ** 2)) / y_avg
-    L.logger(f'The average of {y_var}: {y_avg:.3f} (RMSF: {RMSF:.3f}, max: {max(y):.3f}, min: {min(y):.3f})')
-    if x_unit == ' ns' or x_unit == ' ps':
+    L.logger(
+        f"The average of {y_var}: {y_avg:.3f} (RMSF: {RMSF:.3f}, max: {max(y):.3f}, min: {min(y):.3f})"
+    )
+    if x_unit == " ns" or x_unit == " ps":
         y = list(y)
-        L.logger(f'The maximum of {y_var} occurs at {x[y.index(max(y))]:.3f}{x_unit}.')
-        L.logger(f'The minimum of {y_var} occurs at {x[y.index(min(y))]:.3f}{x_unit}.')
+        L.logger(f"The maximum of {y_var} occurs at {x[y.index(max(y))]:.3f}{x_unit}.")
+        L.logger(f"The minimum of {y_var} occurs at {x[y.index(min(y))]:.3f}{x_unit}.")
         y = np.array(y)
         diff = np.abs(y - y_avg)
         t_avg = x[np.argmin(diff)]
         L.logger(
-            f'The configuration at {t_avg:.3f}{x_unit} has the {y_var} ({y[np.argmin(diff)]:.3f}{y_unit}) that is closet to the average.')
+            f"The configuration at {t_avg:.3f}{x_unit} has the {y_var} \
+                ({y[np.argmin(diff)]:.3f}{y_unit}) that is closet to the average."
+        )
--- MD_plotting_toolkit/plot_xy.py
+++ MD_plotting_toolkit/plot_xy.py
@@ -3,7 +3,7 @@
 #    MD_plotting_toolkit,                                                 #
 #    a python package to visualize the results obtained from MD           #
 #                                                                         #
-#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                       #                                                             #
+#    Written by Wei-Tse Hsu <wehs7661@colorado.edu>                       #
 #    Copyright (c) 2021 University of Colorado Boulder                    #
 #                                                                         #
 ###########################################################################
@@ -18,133 +18,179 @@ import sys
 import data_processing
 import matplotlib.pyplot as plt
 import natsort
-import numpy as np
 import plotting_utils
-from utils import *
+import utils
 
 
 def initialize():
 
     parser = argparse.ArgumentParser(
-        description='This code plots variable y against x given a set of 2d data.')
-    parser.add_argument('-i',
-                        '--input',
-                        nargs='+',
-                        help='The filename(s) of the input(s). Wildcards can be used.')
-    parser.add_argument('-l',
-                        '--legend',
-                        nargs='+',
-                        help='Legends of the curves. Default: No legends.')
-    parser.add_argument('-x',
-                        '--xlabel',
-                        type=str,
-                        default='X-axis',
-                        help='The name and units of x-axis. Default: "X-axis".')
-    parser.add_argument('-y',
-                        '--ylabel',
-                        type=str,
-                        default='Y-axis',
-                        help='The name and units of y-axis. Default: "Y-axis".')
-    parser.add_argument('-c',
-                        '--column',
-                        type=int,
-                        default=1,
-                        help='The column index (starting from 0) of the dependent variable. Default: 1.')
-    parser.add_argument('-t',
-                        '--title',
-                        type=str,
-                        help='Title of the plot. Default: No title.')
-    parser.add_argument('-n',
-                        '--pngname',
-                        type=str,
-                        help='The filename of the figure, not including the extension. \
-                            The default is the filename of the input with .png as the extension.')
-    parser.add_argument('-cx',
-                        '--x_conversion',
-                        choices=['degree to radian', 'radian to degree', 'kT to kcal/mol',
-                                 'kcal/mol to kT', 'kT to kJ/mol', 'kJ/mol to kT', 'kJ/mol to kcal/mol',
-                                 'kcal/mol to kJ/mol', 'ns to ps', 'ps to ns'],
-                        help='The unit conversion for the data in x-axis.')
-    parser.add_argument('-cy',
-                        '--y_conversion',
-                        choices=['degree to radian', 'radian to degree', 'kT to kcal/mol',
-                                 'kcal/mol to kT', 'kT to kJ/mol', 'kJ/mol to kT', 'kJ/mol to kcal/mol',
-                                 'kcal/mol to kJ/mol'],
-                        help='The unit conversion for the data in y-axis.')
-    parser.add_argument('-fx',
-                        '--factor_x',
-                        type=float,
-                        help='The factor to be multiplied to the x values.')
-    parser.add_argument('-fy',
-                        '--factor_y',
-                        type=float,
-                        help='The factor to be multiplied to the y values.')
-    parser.add_argument('-T',
-                        '--temp',
-                        help='Temperature for unit convesion involving kT. Default: 298.15.')
-    parser.add_argument('-tr',
-                        '--truncate',
-                        help='-tr 1 means truncate the first 1%% of the data.')
-    parser.add_argument('-r',
-                        '--retain',
-                        help='-r 1 means only analyze the first 1%% of the data.')
-    parser.add_argument('-lc',
-                        '--legend_col',
-                        type=int,
-                        default=1,
-                        help='The number of columns of the legends.')
-    parser.add_argument('-d',
-                        '--dir',
-                        default='',
-                        help='The output directory. The default is where the command is executed.')
-    parser.add_argument('-o',
-                        '--output',
-                        help='The file name of output documenting the statistics of the input data.')
+        description="This code plots variable y against x given a set of 2d data."
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        nargs="+",
+        help="The filename(s) of the input(s). Wildcards can be used.",
+    )
+    parser.add_argument(
+        "-l", "--legend", nargs="+", help="Legends of the curves. Default: No legends."
+    )
+    parser.add_argument(
+        "-x",
+        "--xlabel",
+        type=str,
+        default="X-axis",
+        help='The name and units of x-axis. Default: "X-axis".',
+    )
+    parser.add_argument(
+        "-y",
+        "--ylabel",
+        type=str,
+        default="Y-axis",
+        help='The name and units of y-axis. Default: "Y-axis".',
+    )
+    parser.add_argument(
+        "-c",
+        "--column",
+        type=int,
+        default=1,
+        help="The column index (starting from 0) of the dependent variable. Default: 1.",
+    )
+    parser.add_argument(
+        "-t", "--title", type=str, help="Title of the plot. Default: No title."
+    )
+    parser.add_argument(
+        "-n",
+        "--pngname",
+        type=str,
+        help="The filename of the figure, not including the extension. \
+                            The default is the filename of the input with .png as the extension.",
+    )
+    parser.add_argument(
+        "-cx",
+        "--x_conversion",
+        choices=[
+            "degree to radian",
+            "radian to degree",
+            "kT to kcal/mol",
+            "kcal/mol to kT",
+            "kT to kJ/mol",
+            "kJ/mol to kT",
+            "kJ/mol to kcal/mol",
+            "kcal/mol to kJ/mol",
+            "ns to ps",
+            "ps to ns",
+        ],
+        help="The unit conversion for the data in x-axis.",
+    )
+    parser.add_argument(
+        "-cy",
+        "--y_conversion",
+        choices=[
+            "degree to radian",
+            "radian to degree",
+            "kT to kcal/mol",
+            "kcal/mol to kT",
+            "kT to kJ/mol",
+            "kJ/mol to kT",
+            "kJ/mol to kcal/mol",
+            "kcal/mol to kJ/mol",
+        ],
+        help="The unit conversion for the data in y-axis.",
+    )
+    parser.add_argument(
+        "-fx",
+        "--factor_x",
+        type=float,
+        help="The factor to be multiplied to the x values.",
+    )
+    parser.add_argument(
+        "-fy",
+        "--factor_y",
+        type=float,
+        help="The factor to be multiplied to the y values.",
+    )
+    parser.add_argument(
+        "-T",
+        "--temp",
+        help="Temperature for unit convesion involving kT. Default: 298.15.",
+    )
+    parser.add_argument(
+        "-tr", "--truncate", help="-tr 1 means truncate the first 1%% of the data."
+    )
+    parser.add_argument(
+        "-r", "--retain", help="-r 1 means only analyze the first 1%% of the data."
+    )
+    parser.add_argument(
+        "-lc",
+        "--legend_col",
+        type=int,
+        default=1,
+        help="The number of columns of the legends.",
+    )
+    parser.add_argument(
+        "-d",
+        "--dir",
+        default="",
+        help="The output directory. The default is where the command is executed.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="The file name of output documenting the statistics of the input data.",
+    )
 
     args_parse = parser.parse_args()
 
     return args_parse
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = initialize()
 
     # Step 1. Setting things up
     plotting_utils.default_settings()
 
     if isinstance(args.input, str):
-        if '*' in args.xvg:  # allow wildcards
+        if "*" in args.xvg:  # allow wildcards
             args.input = natsort.natsorted(glob.glob(args.input), reverse=False)
-        else:    # only one input file
+        else:  # only one input file
             args.input = list(args.xvg)
 
     if args.pngname is None:
-        args.pngname = '.'.join(args.input.split('.')[:-1])  # '.png' will be appended later
+        args.pngname = ".".join(
+            args.input.split(".")[:-1]
+        )  # '.png' will be appended later
 
     if args.temp is None:
         args.temp = 298.15
 
     if args.output is None:
-        args.output = 'results_' + args.pngname.split('.png')[0] + '.txt'
+        args.output = "results_" + args.pngname.split(".png")[0] + ".txt"
 
-    L = Logging(args.dir + args.output)
+    L = utils.Logging(args.dir + args.output)
 
     # Steo 2. Read and preprocess (e.g. deduplication, unit conversion) the input data
     for i in range(len(args.input)):
-        result_str = '\nData analysis of the file: %s' % args.input[i]
+        result_str = "\nData analysis of the file: %s" % args.input[i]
         L.logger(result_str)
-        L.logger('=' * (len(result_str) - 1))  # len(result_str) includes \n
-        L.logger(f'- Working directory: {os.getcwd()}')
+        L.logger("=" * (len(result_str) - 1))  # len(result_str) includes \n
+        L.logger(f"- Working directory: {os.getcwd()}")
         L.logger(f'- Command line: {" ".join(sys.argv)}')
-        L.logger('Analyzing the file ... ')
-        L.logger('Plotting and saving figure ...')
+        L.logger("Analyzing the file ... ")
+        L.logger("Plotting and saving figure ...")
         x, y = data_processing.read_2d_data(args.input[i], args.column)
 
         if args.x_conversion is not None or args.factor_x is not None:
-            x = data_processing.scale_data(x, args.x_conversion, args.factor_x, args.temp)
+            x = data_processing.scale_data(
+                x, args.x_conversion, args.factor_x, args.temp
+            )
 
         if args.y_conversion is not None or args.factor_y is not None:
-            y = data_processing.scale_data(y, args.y_conversion, args.factor_y, args.temp)
+            y = data_processing.scale_data(
+                y, args.y_conversion, args.factor_y, args.temp
+            )
 
         # Data slicing as needed
         x = data_processing.slice_data(x, args.truncate, args.retain)
@@ -157,19 +203,19 @@ if __name__ == '__main__':
         if args.legend is None:
             plt.plot(x, y)
         else:
-            plt.plot(x, y, label=f'{args.legend[i]}')
+            plt.plot(x, y, label=f"{args.legend[i]}")
             if len(args.input) > 1:
                 plt.legend(ncol=args.legend_col)
 
     if args.title is not None:
-        plt.title(f'{args.title}', weight='bold')
-    plt.xlabel(f'{args.xlabel}')
-    plt.ylabel(f'{args.ylabel}')
+        plt.title(f"{args.title}", weight="bold")
+    plt.xlabel(f"{args.xlabel}")
+    plt.ylabel(f"{args.ylabel}")
     if max(abs(x)) >= 10000:
-        plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+        plt.ticklabel_format(style="sci", axis="x", scilimits=(0, 0))
     if max(abs(y)) >= 10000:
-        plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+        plt.ticklabel_format(style="sci", axis="y", scilimits=(0, 0))
     plt.grid(True)
 
-    plt.savefig(f'{args.dir}{args.pngname}.png')
+    plt.savefig(f"{args.dir}{args.pngname}.png")
     plt.show()
